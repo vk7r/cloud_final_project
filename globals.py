d35b0763bf53089bd9ac522d9cc4294d73bfe8bc
vk7r <viktorkangasniemi@gmail.com>
2024-11-08 15:42:26 -0500

    updated globals

--- globals.py
+++ globals.py
@@ -4,8 +4,8 @@ pem_file_path = "/home/arshavin/.aws/key.pem"
 aws_folder_path = "/home/arshavin/.aws"
 
 key_pair_name = "key_pair_name"
-public_security_group_name = "public_security_group_name2424345252565331"
-internal_security_group_name = "internal_security_group_name2542233655524431"
+public_security_group_name = "public_security_group_name2427443445434532552565331"
+internal_security_group_name = "internal_security_group_name2542423954343436555524431"
 
 # Log into MySQL
 # sudo mysql -u root -p
