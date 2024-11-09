dc54ca694154693eb46d839e08b943a2fe9385a9
vk7r <viktorkangasniemi@gmail.com>
2024-11-09 14:37:29 -0500

    added comments

--- globals.py
+++ globals.py
@@ -4,8 +4,11 @@ pem_file_path = "/home/arshavin/.aws/key.pem"
 aws_folder_path = "/home/arshavin/.aws"
 
 key_pair_name = "key_pair_name"
-public_security_group_name = "public_security_group_name2427443445434532552565331"
-internal_security_group_name = "internal_security_group_name2542423954343436555524431"
+public_security_group_name = "public_security_group_name2"
+internal_security_group_name = "internal_security_group_name2"
 
 # Log into MySQL
 # sudo mysql -u root -p
+
+# export FLASK_APP=gateway_app.py
+# flask run --host=0.0.0.0 --port=5000
