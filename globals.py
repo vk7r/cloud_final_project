1e12368b5b71a56096aba6d52bb53ed7c381a8bd
vk7r <viktorkangasniemi@gmail.com>
2024-11-13 17:50:12 -0500

    updated globals

--- globals.py
+++ globals.py
@@ -4,8 +4,8 @@ pem_file_path = "/home/arshavin/.aws/key.pem"
 aws_folder_path = "/home/arshavin/.aws"
 
 key_pair_name = "key_pair_name"
-public_security_group_name = "public_security_group_name43l44o4161l2"
-internal_security_group_name = "internal_security_gro4up4_l31ol46143"
+public_security_group_name = "public_security_g2rou5p_11nam"
+internal_security_group_name = "internal_security_gr21o54u1p4"
 
 GATEKEEPER_PASSWORD = "your_secure_password"
 
