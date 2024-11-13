0065535ff8e4da2c31c8304bf1955c42949ffff9
vk7r <viktorkangasniemi@gmail.com>
2024-11-13 12:44:38 -0500

    refactor: update globals

--- globals.py
+++ globals.py
@@ -4,8 +4,8 @@ pem_file_path = "/home/arshavin/.aws/key.pem"
 aws_folder_path = "/home/arshavin/.aws"
 
 key_pair_name = "key_pair_name"
-public_security_group_name = "public_security_group_name4l4ol2"
-internal_security_group_name = "internal_security_gro4up_lol43"
+public_security_group_name = "public_security_group_name43l44o4161l2"
+internal_security_group_name = "internal_security_gro4up4_l31ol46143"
 
 GATEKEEPER_PASSWORD = "your_secure_password"
 
